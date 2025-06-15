787e94220b6bd889cfb36630a75d539ad3c7db20
myusername <mdsumonhossan743@gmail.com>
2025-06-16 00:26:14 +0600

    done

--- lib/feature/payment/screen/payment.dart
+++ lib/feature/payment/screen/payment.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:inapp_purchase/feature/payment/controller/payment_controller.dart';
 
 class Payment extends StatelessWidget {
   const Payment({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final PaymentController paymentController = Get.put(PaymentController());
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(onPressed: () {}, child: Text("Pay")),
+      ),
+    );
   }
 }
